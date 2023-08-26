42f7ea8bffce4c2cdd8a5587ad57c6d0ed382cbb
NangunooriAbhinav <131000742+NangunooriAbhinav@users.noreply.github.com>
2023-08-26 21:05:17 +0530

    frontend progress

---
+++ index.js
@@ -0,0 +1,14 @@
+/* This JS is for demonstration purpose */
+
+var $last = $('.progress-radial');
+
+setInterval(function () {
+    var currentClass = $last.attr('class').split(' ')[1];
+    var currentPercentage = currentClass.substring(9, 12);
+    var newPercentage = (parseInt(currentPercentage) + 1);
+    if (newPercentage > 100) {
+        newPercentage = 1;
+    }
+    var newClass = 'progress-' + newPercentage;
+    $last.removeClass(currentClass).addClass(newClass);
+}, 100);
